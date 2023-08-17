24fa5377d40890164323c7e90517e307da1be8d8
pavbyte <pavbyte@gmail.com>
2023-08-17 13:00:45 +0530

    CLI - note taking app

---
+++ src/notes.js
@@ -0,0 +1,31 @@
+import { insert, getDB, saveDB } from "./db.js";
+
+export const newNote = async (note, tags) => {
+    const data = {
+        tags,
+        content: note,
+        id: Date.now()
+    }
+    await insert(data)
+    return data
+}
+export const getAllNotes = async () => {
+    const db = await getDB()
+    return db.notes
+}
+export const findNotes = async (filter) => {
+    const notes = await getAllNotes()
+    return notes.filter(note => note.content.toLowerCase().includes(filter.toLowerCase()))
+}
+export const removeNote = async (id) => {
+    const notes = await getAllNotes()
+    const match = notes.filter(note => note.id === id)
+    if (match) {
+        const newNote = notes.filter(note => note.id !== id)
+        await saveDB(newNote)
+        return id
+    }
+}
+export const removeAllNotes = async () => {
+    await saveDB({ notes: [] })
+}
